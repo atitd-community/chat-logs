3320a80871f6bcb438ebffc6d76259cb3b26c78a
Brad Turner <brad@clanofartisans.com>
2021-06-06 16:32:32 -0500

    Temporary version so we can get System log access up and running.

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\ChatLog;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('system/{timestamp}', function(Request $request, $timestamp) {
+    $raw = ChatLog::where('game_name', 'ATITD10A')
+                  ->where('channel_name', 'System')
+                  ->where('msg_date', '>', gmdate("Y-m-d H:i:s", $timestamp))
+                  ->get();
+
+    $return = [];
+
+    foreach($raw as $key => $value) {
+        $return[$value->index_id]['message'] = $value->full_message;
+        $return[$value->index_id]['timestamp'] = (new Carbon($value->msg_date))->timestamp;
+    }
+
+    return $return;
+});
